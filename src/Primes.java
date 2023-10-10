a2ffa5d7031c09a401ac0a499a72e30dd1a64262
mohammad <mehrbod67@gmail.com>
2023-10-10 20:24:56 +0330

    added Language Basics Task

---
+++ src/Primes.java
@@ -0,0 +1,17 @@
+public class Primes {
+    public static void main(String[] args) {
+        int nValues = 50;
+        boolean isPrime = true;
+        for (int i = 2; i <= nValues; i++) {
+            isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+                System.out.println(i);
+        }
+    }
+}
