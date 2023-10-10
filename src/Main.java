7386557d04e62c266168bdad943f72e4828ccaa1
mohammad <mehrbod67@gmail.com>
2023-10-10 19:46:10 +0330

    use string.format instead of math.round

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     System.out.printf("the volume of the sun is: %.2f cubic miles%n", sunVolume);
     double earthVolume = (4.0 / 3.0 ) * Math.PI * (Math.pow(earthDiameter /2 , 3)) ;
-    System.out.println("the volume of the earth is: " +Math.round(earthVolume) + " cubic miles");
+    System.out.printf("the volume of the earth is: %.2f cubic miles%n", earthVolume);
     double ratio = sunVolume / earthVolume ;
         System.out.println("the ratio of sun's volume to earth's volume is: " + Math.round( ratio )) ;
 
