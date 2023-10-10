205ed56cb93d5cd45cf5d583c5207fb90fb61c03
mohammad <mehrbod67@gmail.com>
2023-10-10 19:40:40 +0330

    initializing

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+    double sunDiameter = 865000;
+    double earthDiameter = 7600;
+    double sunVolume = (4.0 / 3.0 ) * Math.PI *(Math.pow(sunDiameter / 2 , 3));
+
+    System.out.printf("the volume of the sun is: %.2f cubic miles%n", sunVolume);
+    double earthVolume = (4.0 / 3.0 ) * Math.PI * (Math.pow(earthDiameter /2 , 3)) ;
+    System.out.println("the volume of the earth is: " +Math.round(earthVolume) + " cubic miles");
+    double ratio = sunVolume / earthVolume ;
+        System.out.println("the ratio of sun's volume to earth's volume is: " + Math.round( ratio )) ;
+
+
+    }
+}
