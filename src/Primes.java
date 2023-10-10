9c81a15486fa3d7d56dcef4f3387dee09b7a1195
mohammad <mehrbod67@gmail.com>
2023-10-10 20:26:28 +0330

    used label and removed isPrime variable

--- src/Primes.java
+++ src/Primes.java
@@ -1,17 +1,17 @@
 public class Primes {
     public static void main(String[] args) {
         int nValues = 50;
-        boolean isPrime = true;
+
+        start:
         for (int i = 2; i <= nValues; i++) {
-            isPrime = true;
-            for (int j = 2; j < i; j++) {
+
+            for (int j = 2; j < i; j++)
+            {
                 if (i % j == 0) {
-                    isPrime = false;
-                    break;
+                    continue start;
                 }
             }
-            if (isPrime)
-                System.out.println(i);
+            System.out.println(i);
         }
     }
 }
